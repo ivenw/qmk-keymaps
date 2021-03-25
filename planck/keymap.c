e3404205c848acd0f7770925a90c003a9aa1bcfa
Iven Winkelmann <winkelmann.iven@gmail.com>
2021-03-25 18:39:38 +0100

    enter on space lower

--- planck/keymap.c
+++ planck/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, KC_PEQL, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_BSPC,
     _______, _______, _______, _______, _______, _______, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PMNS, _______,
     _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_PSLS, KC_ENT,
-    _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
- _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,   _______,   KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+ _______, _______, _______, _______, _______, KC_BSPC, KC_BSPC,   _______,   KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Plover layer (http://opensteno.org)
